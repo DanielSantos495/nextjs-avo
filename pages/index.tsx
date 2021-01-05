e5b8ca7005e36b571ccc8210a4e63882d3a3ada1
Daniel Santos <dsantos0495@gmail.com>
2021-01-05 10:02:55 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import Layout from '@components/Layout/Layout'
+import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
+import ProductList from '@components/ProductList/ProductList'
+import fetch from 'isomorphic-unfetch'
+
+const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+
+  useEffect(() => {
+    fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data }: TAPIAvoResponse) => {
+        setProductList(data)
+      })
+  }, [])
+
+  return (
+    <Layout>
+      <KawaiiHeader />
+      <ProductList products={productList} />
+    </Layout>
+  )
+}
+
+export default HomePage
